53093f23b473ee7458e428b2b1a988583247663f
tosih <sohail@runnable.com>
2016-10-04 16:12:09 -0700

    Add error logging and handler.

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ const app = express()
 
 app.use(bodyParser.json())
 app.use('/', routes)
+app.use(logErrors)
+app.use(errorHandler)
+
+function logErrors (err, req, res, next) {
+  console.error(err.stack)
+  next(err)
+}
+
+function errorHandler (err, req, res, next) {
+  res.status(500)
+  res.render('error', { error: err })
+}
 
 const server = app.listen(3000, () => {
   const {address, port} = server.address()
